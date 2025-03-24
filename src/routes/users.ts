1d3ab604681408f01545c31167fd0e2d1fda6d60
Lukas Beyer <lukas.beyer@juicymo.cz>
2025-03-24 21:56:07 +0100

    Add POST /user endpoint

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import Express, { Router } from "express";
 import { AppDataSource } from "../database";
 import Users from "../entity/users";
 
@@ -9,5 +9,26 @@ router.get("/users", async (req, res) => {
   const usersArray = await users.find();
   res.json({ status: "OK", data: usersArray });
 });
+
+router.route("/user")
+  .post(async (req, res) => {
+    const { firstName, lastName, username, email, age, shoeSize, hairColor, familyMembers } = req.body;
+    const user = Users.create({
+      first_name: firstName,
+      last_name: lastName,
+      username,
+      email,
+      age,
+      additional_info: {
+        shoeSize,
+        hairColor
+      },
+      family_members: familyMembers
+    });
+
+    await user.save();
+
+    res.json(user);
+  })
 
 export default router;
