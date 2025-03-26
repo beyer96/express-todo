b3285a9c3df1718e0a350563ed2cafa9f7082d0d
Lukas Beyer <lukas.beyer@juicymo.cz>
2025-03-26 13:22:36 +0100

    Create users table

---
+++ src/database/migrations/1742989023237-CreateUsersTable.ts
@@ -0,0 +1,46 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUsersTable1742989023237 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const usersTable = new Table({
+      name: "users",
+      columns: [
+        {
+          name: "id",
+          type: "int",
+          isPrimary: true
+        },
+        {
+          name: "first_name",
+          type: "varchar",
+        },
+        {
+          name: "last_name",
+          type: "varchar",
+        },
+        {
+          name: "username",
+          type: "varchar",
+          isUnique: true
+        },
+        {
+          name: "email",
+          type: "varchar",
+          isUnique: true
+        },
+        {
+          name: "password",
+          type: "text"
+        }
+      ]
+    });
+
+    await queryRunner.createTable(usersTable);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users", true);
+  }
+
+}
