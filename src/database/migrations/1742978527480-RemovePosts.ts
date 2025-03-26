e33d68ee00ee1a2a7a482b8ffb2289c148118c1e
Lukas Beyer <lukas.beyer@juicymo.cz>
2025-03-26 13:15:05 +0100

    Fix indentation

--- src/database/migrations/1742978527480-RemovePosts.ts
+++ src/database/migrations/1742978527480-RemovePosts.ts
@@ -2,42 +2,42 @@ import { MigrationInterface, QueryRunner, Table, TableForeignKey } from "typeorm
 
 export class RemovePosts1742978527480 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable("posts", true);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("posts", true);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        const posts = new Table({
-          name: "posts",
-          columns: [
-            {
-              name: "id",
-              type: "int",
-              isPrimary: true,
-            },
-            {
-              name: "title",
-              type: "varchar",
-            },
-            {
-              name: "text",
-              type: "text",
-            },
-            {
-              name: "user_id",
-              type: "int",
-              isNullable: false,
-            }
-          ]
-        });
-        const fkUser = new TableForeignKey({
-          columnNames: ["user_id"],
-          referencedColumnNames: ["id"],
-          referencedTableName: "users",
-          onDelete: "CASCADE"
-        })
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const posts = new Table({
+      name: "posts",
+      columns: [
+        {
+          name: "id",
+          type: "int",
+          isPrimary: true,
+        },
+        {
+          name: "title",
+          type: "varchar",
+        },
+        {
+          name: "text",
+          type: "text",
+        },
+        {
+          name: "user_id",
+          type: "int",
+          isNullable: false,
+        }
+      ]
+    });
+    const fkUser = new TableForeignKey({
+      columnNames: ["user_id"],
+      referencedColumnNames: ["id"],
+      referencedTableName: "users",
+      onDelete: "CASCADE"
+    })
 
-        await queryRunner.createTable(posts);
-        await queryRunner.createForeignKey(posts, fkUser)
-    }
+    await queryRunner.createTable(posts);
+    await queryRunner.createForeignKey(posts, fkUser)
+  }
 }
