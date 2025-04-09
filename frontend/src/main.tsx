43563ba39b68a02f21bf04673f4bfb9f9f954425
Lukas Beyer <lukas.beyer@juicymo.cz>
2025-04-09 17:33:48 +0200

    Move import

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
 
 import "./stylesheets/index.scss";
-import { RouterProvider } from "react-router";
 import router from "./router";
 
 createRoot(document.getElementById("root")!).render(
