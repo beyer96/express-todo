e5a7388dde854e9cf98b65b5a571930b91ea5a92
Lukas Beyer <lukas.beyer@juicymo.cz>
2025-04-11 22:17:49 +0200

    Fix dashboard task stats

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,18 +1,32 @@
+import { useEffect } from "react";
 import { LuListTodo, LuFolderGit, LuClipboardCheck } from "react-icons/lu";
-import { useAppSelector } from "../store"
+import { useAppDispatch, useAppSelector } from "../store"
 import Stat from "../components/Stat";
 import { IconContext } from "react-icons/lib";
+import TasksService from "../services/tasksService";
+import { setTasks } from "../store/tasksSlice";
 
 export default function Home() {
+  const dispatch = useAppDispatch();
   const user = useAppSelector(state => state.user);
   const tasks = useAppSelector(state => state.tasks);
+
+  useEffect(() => {
+    if (!user.username) return;
+
+    (async () => {
+      const tasks = await TasksService.getTasks();
+  
+      dispatch(setTasks(tasks));
+    })();
+  }, [dispatch, user.username]);
 
   if (user.username) {
     return (
       <>
         <h1 className="text-center mt-5">Welcome, {user.username}!</h1>
         <div className="d-flex justify-content-center my-5 gap-5">
-          <Stat title="Tasks to be done" value={tasks.length}>
+          <Stat title="Tasks to be done" value={tasks.filter(task => !task.is_done).length}>
             <IconContext.Provider value={{ size: "6rem" }}>
               <LuListTodo />
             </IconContext.Provider>
@@ -22,7 +36,7 @@ export default function Home() {
               <LuFolderGit />
             </IconContext.Provider>
           </Stat>
-          <Stat title="Done tasks" value={1502}>
+          <Stat title="Done tasks" value={tasks.filter(task => task.is_done).length}>
             <IconContext.Provider value={{ size: "6rem" }}>
               <LuClipboardCheck />
             </IconContext.Provider>
